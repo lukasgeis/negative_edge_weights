2e07e9d4ef085bb92e9d4f3987f5dd2d480f585b
Lukas Geis <lukasgeis@gmx.de>
2024-07-15 12:31:45 +0200

    luxembourg data instead of berlin

--- exp/acceptance/roads.sh
+++ exp/acceptance/roads.sh
@@ -5,11 +5,9 @@ cargo build --release --features acceptance
 
 OUTPUT="data/acceptance"
 
-mkdir -p "$OUTPUT/roads/be"
-mkdir -p "$OUTPUT/roads/bc"
+mkdir -p "$OUTPUT/roads/"
 
-echo "round,rate,initial,degree" > "$OUTPUT/roads-be.out"
-echo "round,rate,initial,degree" > "$OUTPUT/roads-bc.out"
+echo "round,rate,initial,degree" > "$OUTPUT/roads.out"
 
 while getopts r: flag
 do
@@ -20,21 +18,15 @@ done
 
 for NUM in {1..10}
 do
-    # Berlin
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i m file -p "exp/roads_data/berlin.edges" >> "$OUTPUT/roads/be/m_$NUM.out" &
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i u file -p "exp/roads_data/berlin.edges" >> "$OUTPUT/roads/be/u_$NUM.out" &
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i z file -p "exp/roads_data/berlin.edges" >> "$OUTPUT/roads/be/z_$NUM.out" &
-
-    # Berlin Contracted
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i m file -p "exp/roads_data/berlin-contracted.edges" >> "$OUTPUT/roads/bc/m_$NUM.out" &
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i u file -p "exp/roads_data/berlin-contracted.edges" >> "$OUTPUT/roads/bc/u_$NUM.out" &
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i z file -p "exp/roads_data/berlin-contracted.edges" >> "$OUTPUT/roads/bc/z_$NUM.out" &
+    # Luxembourg Contracted
+    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i m file -p "exp/roads_data/luxembourg-contracted.edges" >> "$OUTPUT/roads/m_$NUM.out" &
+    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i u file -p "exp/roads_data/luxembourg-contracted.edges" >> "$OUTPUT/roads/u_$NUM.out" &
+    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -i z file -p "exp/roads_data/luxembourg-contracted.edges" >> "$OUTPUT/roads/z_$NUM.out" &
 done
 
 wait
 
-cat $OUTPUT/roads/be/* >> "$OUTPUT/roads-be.out"
-cat $OUTPUT/roads/bc/* >> "$OUTPUT/roads-bc.out"
+cat $OUTPUT/roads/* >> "$OUTPUT/roads.out"
 
 rm -r "$OUTPUT/roads"
 
--- exp/intervals/roads.sh
+++ exp/intervals/roads.sh
@@ -5,12 +5,10 @@ cargo build --release --features intervals
 
 OUTPUT="data/intervals"
 
-mkdir -p "$OUTPUT/roads/be"
-mkdir -p "$OUTPUT/roads/bc"
+mkdir -p "$OUTPUT/roads"
 
 
-echo "round,avg,frac,time,algo"> "$OUTPUT/roads-be.out"
-echo "round,avg,frac,time,algo"> "$OUTPUT/roads-bc.out"
+echo "round,avg,frac,time,algo"> "$OUTPUT/roads.out"
 
 while getopts r: flag
 do
@@ -21,19 +19,14 @@ done
 
 for NUM in {1..10}
 do
-    # Berlin
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a bd file -p "exp/roads_data/berlin.edges" >> "$OUTPUT/roads/be/bd_$NUM.out"
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a d file -p "exp/roads_data/berlin.edges" >> "$OUTPUT/roads/be/d_$NUM.out"
-
-    # Berlin Contracted
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a bd file -p "exp/roads_data/berlin-contracted.edges" >> "$OUTPUT/roads/bc/bd_$NUM.out"
-    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a d file -p "exp/roads_data/berlin-contracted.edges" >> "$OUTPUT/roads/bc/d_$NUM.out"
+    # Luxembourg Contracted
+    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a bd file -p "exp/roads_data/luxembourg-contracted.edges" >> "$OUTPUT/roads/bd_$NUM.out" &
+    ./target/release/random_negative_weights -w=-100 -W 100 -r $ROUNDS -t f64 -a d file -p "exp/roads_data/luxembourg-contracted.edges" >> "$OUTPUT/roads/d_$NUM.out" &
 done
 
 wait
 
-cat $OUTPUT/roads/be/* >> "$OUTPUT/roads-be.out"
-cat $OUTPUT/roads/bc/* >> "$OUTPUT/roads-bc.out"
+cat $OUTPUT/roads/* >> "$OUTPUT/roads.out"
 
 rm -r "$OUTPUT/roads"
 
